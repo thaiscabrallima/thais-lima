7fd68a793b7dd171ecbcf007b218ac226a38c540
Thais Cabral <thaiscblima@gmail.com>
2018-07-26 09:42:07 -0400

    contact form

--- js/main.js
+++ js/main.js
@@ -6,3 +6,15 @@ $(document).ready(function(){
     })
 
 });
+
+function frmfunction() {
+    var firstName = document.getElementsByName('fname')[0].value;
+    var lastName = document.getElementsByName('lname')[0].value;
+    var email = document.getElementsByName('email')[0].value;
+    var message = document.getElementsByName('message')[0].value;
+    console.log(firstName);
+    console.log(lastName);
+    console.log(email);
+    console.log(message);
+}
+
